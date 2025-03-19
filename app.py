c1a48761ca4830957400aa2623b0c0dd7115c006
Anup Maharjan <88359432+anopmhj77@users.noreply.github.com>
2025-03-19 22:03:01 +0545

    app.py

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ col1, col2, col3, col4 = st.columns([1, 1, 2, 1])
 with col1:
     st.write("")
 with col2:
-    st.image("E:/project/profoilo/assets/me.png", width=100)
+    st.image("me.png", width=100)
 with col3:
     st.title("Anup Maharjan")
     st.header("Data Scientist")
